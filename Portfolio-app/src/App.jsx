2d6988d9dc4c5c1d366f9e58c50e2c07803e250b
figoria <1056443@hr.nl>
2025-11-14 21:34:53 +0100

    start of design

--- Portfolio-app/src/App.jsx
+++ Portfolio-app/src/App.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useNavigate } from "react-router-dom";
+
 
 function App() {
   const [count, setCount] = useState(0)
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <>
+            <div style={{
+                display: "flex",
+                gap: "20px",          // ruimte tussen knoppen
+                padding: "20px",      // ruimte boven/omheen
+                justifyContent: "flex-start", // links uitlijnen
+            }}>
+                <button onClick={() => navigate("/")}>
+                    Home
+                </button>
+                <button onClick={() => navigate("/")}>
+                    Over mij
+                </button>
+                <button onClick={() => navigate("/")}>
+                    Projecten
+                </button>
+                <button onClick={() => document.getElementById("contact").scrollIntoView({behavior: "smooth"})}>
+                    Contact
+                </button>
+            </div>
+
+            <h1>Vite + React</h1>
+            <div className="card">
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+            <p>
+
+            </p>
+            <p>
+
+            </p>
+            <p></p>
+
+            <div id="contact">
+                <h2>Contact sectie</h2>
+            </div>
+        </>
+    )
 }
 
 export default App
