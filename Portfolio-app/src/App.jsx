eb5c6176bdb2d3babd2b2704940c561750f91115
figoria <sambijkerk@gmail.com>
2025-11-16 21:07:31 +0100

    added homepage

--- Portfolio-app/src/App.jsx
+++ Portfolio-app/src/App.jsx
@@ -1,57 +1,69 @@
 import { useState } from 'react'
 import './App.css'
 import { useNavigate } from "react-router-dom";
+import foto from "./assets/images/foto.png"
 
 
 function App() {
-  const [count, setCount] = useState(0)
     const navigate = useNavigate();
 
 
     return (
-        <>
-            <div style={{
-                display: "flex",
-                gap: "20px",          // ruimte tussen knoppen
-                padding: "20px",      // ruimte boven/omheen
-                justifyContent: "flex-start", // links uitlijnen
-            }}>
-                <button onClick={() => navigate("/")}>
-                    Home
-                </button>
-                <button onClick={() => navigate("/")}>
-                    Over mij
-                </button>
-                <button onClick={() => navigate("/")}>
-                    Projecten
-                </button>
-                <button onClick={() => document.getElementById("contact").scrollIntoView({behavior: "smooth"})}>
-                    Contact
-                </button>
-            </div>
+        <div className="container">
 
-            <h1>Vite + React</h1>
-            <div className="card">
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
+            <nav className="navbar">
+                <button>Home</button>
+                <button>Over mij</button>
+                <button>Projecten</button>
+                <button onClick={() =>
+                    document.getElementById("contact").scrollIntoView({behavior: "smooth"})
+                }>Contact</button>
+            </nav>
+
+            <section className="hero">
+                <div className="hero-text">
+                    <h1 className="name">Sam<br />Bijkerk</h1>
+                    <p className="subtitle">Creative developer</p>
+                </div>
+
+                <img src={foto} alt="foto van Sam" className="hero-img"/>
+            </section>
+
+            <section className="about" id="about">
+                <h2>Hello world</h2>
+                <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat."
                 </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-            <p>
+            </section>
 
-            </p>
-            <p>
+            <section className="projects" id="projects">
+                <div className="project project1"></div>
+                <div className="project project2"></div>
+                <div className="project project3"></div>
+                <div className="project project4"></div>
+            </section>
 
-            </p>
-            <p></p>
+            <section className="contact" id="contact">
+                <h2>Contact</h2>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat."
+                </p>
 
-            <div id="contact">
-                <h2>Contact sectie</h2>
-            </div>
-        </>
-    )
+                <div className="socials">
+                    <div className="icon"></div>
+                    <div className="icon"></div>
+                    <div className="icon"></div>
+                    <div className="icon"></div>
+                </div>
+            </section>
+
+        </div>
+    );
 }
 
 export default App
